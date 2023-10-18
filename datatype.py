5f57dc6c112f3ccc4354089b3932d64a27f51d39
Sonani Jemish <sonanijemish2002@gmail.com>
2023-10-18 19:53:07 +0530

    1.add operator file

--- datatype.py
+++ datatype.py
@@ -16,4 +16,10 @@ list =  [8,8.2,[-4,5],"apple"]  #here you cad add different type of value and sa
 print("list value : ",list,end="\n\n")
 
 dictionary = {"name":"Jemish","age":"21","gender":"male"}
-print(dictionary)
+print(dictionary,end="\n\n")
+
+
+# Convert datatype into different datatype
+a="1"
+b="2.5"
+print(int(a)+float(b))  #also you can convert into different datatype using : int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict()
---
+++ operator.py
@@ -0,0 +1,7 @@
+print(1+2)  #addition
+print(12-6) #minus
+print(1*5)  #multiplication
+print(4/2)  #division
+print(5//3) #floor division ex: 5/3 = 1.66 but print "1"
+print(5%3)  #modulus - show reminder
+print(5**3) #exponential - multiply first value sencond value time ( Ex: 5**3 --5 multiplyby 3 time and result 125 )
