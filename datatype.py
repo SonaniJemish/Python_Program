eed19ea5a6e94e7a648d229babc36035be1b7a44
Sonani Jemish <sonanijemish2002@gmail.com>
2023-10-18 18:55:51 +0530

    datatype

---
+++ datatype.py
@@ -0,0 +1,12 @@
+a = 1   # this is int type
+b = "Jemish"    # this is str type
+c = True    # this is bool type
+d = None    # this is none type
+print(a)
+print(b)
+print(c)
+print(d)
+print("Type of a : ",type(a))
+print("Type of b : ",type(b))
+print("Type of c : ",type(c))
+print("Type of d : ",type(d))
