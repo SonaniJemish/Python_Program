e3520a3cf9f3713a07ab50d969790ba32cd8afc2
Sonani Jemish <sonanijemish2002@gmail.com>
2023-10-21 01:20:58 +0530

    function added

--- create_function.py
+++ create_function.py
@@ -14,3 +14,19 @@ a=2
 b=3
 sum(a,b)
 sum(9,1)
+
+
+
+
+
+#dictionary ke liye (**name)  use hota hai
+# simple way me (a,b) aisa likh sakte hai
+
+def average (*num):                  
+    sum=0
+    for i in num:
+        sum=sum+i
+    return sum/len(num)
+
+ans = average(6,6,6,6,6,6)
+print("Average : ",ans)
