e9fc0cd8a0af266df5788c5bc2408d8100868c52
Sonani Jemish <sonanijemish2002@gmail.com>
2023-10-20 19:56:22 +0530

    loops

--- for_loop.py
+++ for_loop.py
@@ -1,3 +1,22 @@
 name = "Jemish"
 for i in name : 
+    print(i, end=", ")
+print("\n\n\n")
+
+
+
+colors = ["Red","Blue","Orange","White"]
+for i in colors:
     print(i)
+    for j in i:
+        print(j)
+
+print("\n\n\n")
+
+
+for s in range(10):
+    print(s+1)
+for s in range(1,10):
+    print(s)
+for s in range(1,10,2): #third value plus hoti hai privious me
+    print(s)
---
+++ while_loop.py
@@ -0,0 +1,20 @@
+i=0
+while(i<5):
+    print(i)
+    i=i+1
+
+j=0
+while(j<=50):
+    j=int(input("Enter value of j : "))
+    print(j)
+
+
+
+# do while example 
+
+i=0
+while True:
+    print(i)
+    i=i+1
+    if(i%100 ==0):
+        break
