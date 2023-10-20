30e3bd5bb2042686cc57fb8a6e23630e357cda8a
Sonani Jemish <sonanijemish2002@gmail.com>
2023-10-20 20:06:15 +0530

    function create

---
+++ create_function.py
@@ -0,0 +1,16 @@
+# syntex
+
+# def function_name (parameter):
+#     pass    ----use when condition not define
+#     code or statement
+
+
+def sum (a,b):
+    eq = a+b
+    print("Sum = ", eq)
+
+
+a=2
+b=3
+sum(a,b)
+sum(9,1)
