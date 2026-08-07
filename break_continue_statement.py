0343e7a2faa855c89b30d805a7ed455a5a72e729
sonaniubantu <sonaniubantu@gmail.com>
2026-08-07 10:59:49 +0530

    Refine code so user can undderstand easily from output

--- break_continue_statement.py
+++ break_continue_statement.py
@@ -1,16 +1,19 @@
 #break statement
-
+print("Break Statement Program Start : \n")
 for i in range(15):
     if(i==10):
+        print("Skip after all argument 11 to 15 because break at i==10")
         break
     print("8 x ",i+1," = ",8*(i+1))
 
 
+print("\n\n\n")
 
 #continue statement
 
+print("Continue Statement Program Start : \n")
 for i in range(15):
     if(i==10):
-        print("Skip i = 10 condition but next continue")
+        print("Skip i = 11 condition but next continue like 12,13,14, and 15")
         continue
     print("8 x ",i+1," = ",8*(i+1))
