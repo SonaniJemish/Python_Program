e60ec6dba7066cf8c2c34278666e1abb84dcd01f
sonaniubantu <sonaniubantu@gmail.com>
2026-08-07 10:59:07 +0530

    File name change and refine code so user can undderstand easily from output

---
+++ arithmatic_operator.py
@@ -0,0 +1,9 @@
+print("Arithmatic operator :\n")
+
+print("Addition =>\n 1 + 2 : ", (1+2),"\n")  #addition
+print("Minus =>\n 12 - 6 : ", 12-6,"\n") #minus
+print("Multiplication =>\n 2 * 5 : ", 2*5,"\n")  #multiplication
+print("Division =>\n 5 / 3 : ", 5/3,"\n")  #division
+print("Floor Division =>\n 5 / 3 : ", 5//3,"\n") #floor division ex: 5/3 = 1.66 but print "1"
+print("Modulus =>\n 5 % 3 : ", 5%3,"\n")  #modulus - ex: 5/3 = 2 so it is print "2"
+print("Exponential =>\n 3**3 : ", 3**3 ,"\n") #exponential - ex: 3**3 -- So it is work like 3*3*3 = 9, Print "9s"
