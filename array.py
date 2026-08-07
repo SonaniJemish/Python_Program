dbb9c5002d56c20b612fa1cdb4d3b9c9a4bdcd03
sonaniubantu <sonaniubantu@gmail.com>
2026-08-07 11:03:36 +0530

    Refine code so user can undderstand easily from output

--- array.py
+++ array.py
@@ -1,9 +1,15 @@
 # index hamesha 0 thi start thase
 
-name = "jemish"
+name = "JEMISH"
+
+print(f"String is : {name} \n")
+
 print(name[0])
 print(name[1])
 print(name[2])
 print(name[3])
 print(name[4])
 print(name[5])
+
+
+print("\n\nNOTE : Index will strat from 0 and 'JEMISH' have 5 index")
