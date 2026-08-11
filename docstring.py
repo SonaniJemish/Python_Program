7df33689145b8f6fb86e240d653974136b13dc92
sonaniubantu <sonaniubantu@gmail.com>
2026-08-12 00:32:29 +0530

    - Refine code so user can undderstand easily from output

--- docstring.py
+++ docstring.py
@@ -1,8 +1,8 @@
 # doc sting always write below first line of function
 
 def square(n):
-    '''take one number ,return square of n'''
-    print("Square of ",n," : ",n**2)
+    """take one number ,return square of n"""
+    print(f"Square of {n} : ",n**2)
 
 square(6)
 
