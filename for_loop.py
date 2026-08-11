2ca96bc48c70ddf207ee49d110f574b2e7206ded
sonaniubantu <sonaniubantu@gmail.com>
2026-08-12 00:57:21 +0530

    - Refine code so user can undderstand easily from output

--- for_loop.py
+++ for_loop.py
@@ -1,7 +1,12 @@
 name = "Jemish"
 for i in name : 
     print(i, end=", ")
-print("\n\n\n")
+
+print("\n")
+
+for i in reversed(name) :
+    print(i, end=", ")
+print("\n\n","-"*100,"\n\n")
 
 
 
@@ -10,13 +15,19 @@ for i in colors:
     print(i)
     for j in i:
         print(j)
+    print("\n")
 
-print("\n\n\n")
+print("-"*100,"\n\n")
 
 
 for s in range(10):
     print(s+1)
+print("-"*100,"\n\n")
+
 for s in range(1,10):
     print(s)
+print("-"*100,"\n\n")
+
 for s in range(1,10,2): #third value plus hoti hai privious me
     print(s)
+print("-"*100,"\n\n")
