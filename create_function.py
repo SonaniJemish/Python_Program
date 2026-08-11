9654158b432b5ebbbf0396cb443b8b93ece6446d
sonaniubantu <sonaniubantu@gmail.com>
2026-08-11 22:38:46 +0530

    Refine code so user can undderstand easily from output

--- create_function.py
+++ create_function.py
@@ -5,28 +5,34 @@
 #     code or statement
 
 
-def sum (a,b):
+def sum_of_two_num (a,b):
     eq = a+b
-    print("Sum = ", eq)
+    print(f"a + b = {eq}")
+
 
 
 a=2
 b=3
-sum(a,b)
-sum(9,1)
+sumation = sum_of_two_num(a,b)
+
+
+# Note : Sum is None because function doesn't return anything so we can not store any value in variable
+print(f"\n\n a = {a} and b = {b} : Sum = {sumation}\n Note : Sum is None because function doesn't return anything \n\n")  #It returns None because the function doesn't return anything.
+
+sum_of_two_num(9,1)
 
 
 
 
 
-#dictionary ke liye (**name)  use hota hai
-# simple way me (a,b) aisa likh sakte hai
+# (*number) shows argument take only numbera
+# simple way me (a,b) aisa likh sakte hai  like "def average (a,b):"
 
-def average (*num):                  
+def average (*number):
     sum=0
-    for i in num:
+    for i in number:
         sum=sum+i
-    return sum/len(num)
+    return sum/len(number)
 
-ans = average(6,6,6,6,6,6)
-print("Average : ",ans)
+avg_ans = average(1,2,3,4,5,6)
+print("Average : ",avg_ans)
